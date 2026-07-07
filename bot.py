83f6b75e5138247de3744e7cbeea7d9cfd8ddfcb
uxuxx <jsrhfjdsrhjfhtddctmjsrngxxjrgs@gmail.com>
2026-07-07 23:02:00 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,10 +4,9 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.filters import CommandStart
 from openai import AsyncOpenAI
 
-TELEGRAM_BOT_TOKEN = "8821540792:AAENUINvuKizYlQYjbxkQZY7H_3oc2ae8Ec"
-ALLOWED_USER_ID = 297562307
-
-DEEPSEEK_API_KEY = "sk-af208aaa58834963b2773a275ac2d537"
+TELEGRAM_BOT_TOKEN = "8821540792:AAENUINvuKizYlQYjbxkQZY7H_38Ec"
+ALLOWED_USER_ID = 2975623
+DEEPSEEK_API_KEY = "sk-af208aaa58834963b2773a27d537"
 DEEPSEEK_BASE_URL = "https://deepseek.com"
 AI_MODEL_NAME = "deepseek-chat"
 
